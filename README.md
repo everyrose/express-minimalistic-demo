bd034de2aca28ea878c246f68198db3087bd5b20
Oleksii Malyshkin <a.malyshkin@busylamp.com>
2021-06-07 14:24:15 +0300

    add api and desc

---
+++ README.md
@@ -0,0 +1,23 @@
+# Express js minimal demo
+
+Serves basic minimalistic express js api.
+
+### How to run:
+
+```
+npm i
+npm run start
+```
+
+### Swagger URL
+
+http://localhost:3000/api-docs
+
+### API
+
+* GET http://localhost:3000/pet - get all pets
+* POST http://localhost:3000/pet  - create new pet
+* PUT http://localhost:3000/pet - update existing pet
+
+* GET http://localhost:3000/pet/petId - get pet by id
+* DELETE http://localhost:3000/pet/petId - delete pet by id
--- app.js
+++ app.js
@@ -24,6 +24,40 @@ app.post('/pet', (req, res) => {
     res.send(req.body);
 })
 
+app.put('/pet', (req, res) => {
+    const id = req.body.id;
+    const index = pets.findIndex(p => p.id === id);
+    if (index === -1) {
+        console.log('404')
+        res.status(404);
+        res.send();
+    } else {
+        pets[index] = req.body;
+        res.send(req.body);
+    }
+})
+
+app.get('/pet/:id', (req, res) => {
+    const id = req.params.id;
+    const pet = pets.find(p => p.id === id);
+    if (pet) {
+        res.json(pet);
+    } else {
+        res.status(404).send();
+    }
+})
+
+app.delete('/pet/:id', (req, res) => {
+    const id = req.params.id;
+    const index = pets.findIndex(p => p.id === id);
+    if (index < 0) {
+        res.status(404).send();
+    } else {
+        pets.splice(index, 1);
+        res.status(200).send();
+    }
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
